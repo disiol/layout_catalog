8e782d90ad61db1ad5fbcfac4b9b696f75df714e
denisimus <deoniisii@gmail.cim>
2025-02-26 16:52:29 +0200

    make build

---
+++ index.b85a0835.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".product-card[data-qa='card']");document.addEventListener("DOMContentLoaded",()=>{!function(t,d){let n=document.getElementById(t);if(n&&e){e.remove();for(let t=0;t<8;t++){let t=e.cloneNode(!0);n.appendChild(t)}}}("cards-container",0)});
+//# sourceMappingURL=index.b85a0835.js.map
